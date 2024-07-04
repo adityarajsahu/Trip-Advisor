94d527e67aa032cc7f1c63ef084c681b323a1129
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2024-07-05 04:42:39 +0530

    basic styling done

--- client/App.js
+++ client/App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+    StyleSheet,
+    Text,
+    View,
+    KeyboardAvoidingView,
+    Platform,
+    FlatList,
+} from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <StatusBar style="light" />
+            <View style={styles.headerContainer}>
+                <Text style={styles.headerText}>Trip Advisor AI</Text>
+            </View>
+            <KeyboardAvoidingView
+                behavior={Platform.OS === "ios" ? "padding" : "height"}
+                style={styles.keyboardAvoidingContainer}
+            >
+                <FlatList />
+            </KeyboardAvoidingView>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#212121",
+        alignItems: "center",
+    },
+    headerContainer: {
+        width: "100%",
+        height: "12%",
+        backgroundColor: "#171717",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    headerText: {
+        color: "#ECECDF",
+        fontSize: 30,
+        fontWeight: "bold",
+        padding: "5%",
+        marginTop: "7%",
+    },
+    keyboardAvoidingContainer: {
+        flex: 1,
+    },
 });
